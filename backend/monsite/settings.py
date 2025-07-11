cf50fb757001ece4bfc770993fabffd84f2a3c97
DROIDINOU <76789720+DROIDINOU@users.noreply.github.com>
2025-07-11 22:20:22 +0200

    deploy

--- backend/monsite/settings.py
+++ backend/monsite/settings.py
@@ -108,4 +108,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Répertoire où Django va collecter les fichiers statiques en production
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- backend/monsite/urls.py
+++ backend/monsite/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('veille.urls')),
 ]
+
+# Ajouter cette ligne pour servir les fichiers statiques
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

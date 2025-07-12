9e2d8b7235661f146f83695c73bd5d68a43c308f
DROIDINOU <76789720+DROIDINOU@users.noreply.github.com>
2025-07-12 05:42:21 +0200

    deploy

--- backend/monsite/urls.py
+++ backend/monsite/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,7 @@ urlpatterns = [
 # Ajouter cette ligne pour servir les fichiers statiques
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    re_path(r'^teststatic/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
